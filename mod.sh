01cb0bb6f3e695d62eece7ff18173f4a264b5593
qpzr <58745620+qpzr@users.noreply.github.com>
2021-03-24 21:12:57 +0000

    Create mod.sh

--- mod.sh
+++ mod.sh
@@ -12,6 +12,7 @@ easylist=(
   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt"
   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt"
   "https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt"
+  "https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/combined_disguised_trackers.txt"
 )
 
 hosts=(
