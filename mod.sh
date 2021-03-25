53cc8b21aad30c64699c611f6096dcbdb06ccaeb
qpzr <58745620+qpzr@users.noreply.github.com>
2021-03-25 23:21:52 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -33,6 +33,8 @@ strict_hosts=(
 dead_hosts=(
   "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/domains.dead.txt"
   "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/hostnames.dead.txt"
+  "https://raw.githubusercontent.com/neodevpro/dead-allow/master/deadallow"
+  "https://raw.githubusercontent.com/FusionPlmH/dead-block/master/deadblock"
 )
 
 rm -f ./origin-files/easylist*
