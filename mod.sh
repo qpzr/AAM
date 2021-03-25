860ec2742b2eb9eb584b4defb80dab2a27b46646
qpzr <58745620+qpzr@users.noreply.github.com>
2021-03-25 16:43:45 -0500

    Create mod.sh

--- mod.sh
+++ mod.sh
@@ -31,7 +31,6 @@ strict_hosts=(
 dead_hosts=(
   "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/domains.dead.txt"
   "https://raw.githubusercontent.com/notracking/hosts-blocklists-scripts/master/hostnames.dead.txt"
-  "https://raw.githubusercontent.com/neodevpro/dead-allow/master/deadallow"
   "https://raw.githubusercontent.com/FusionPlmH/dead-block/master/deadblock"
 )
 
