352425986a1e91213fa7d8774101091ab7a3bfbb
qpzr <58745620+qpzr@users.noreply.github.com>
2021-05-10 06:17:42 -0500

    Create mod.sh

--- mod.sh
+++ mod.sh
@@ -40,12 +40,6 @@ rm -f ./origin-files/dead-hosts*
 
 cp ./origin-files/some-else.txt ./origin-files/dead-hosts444.txt
 
-curl --connect-timeout 60 -s -o - https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanAD.list \
- | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' >./origin-files/hosts998.txt
-
-curl --connect-timeout 60 -s -o - https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanProgramAD.list \
- | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' >./origin-files/hosts999.txt
-
 for i in "${!easylist[@]}"
 do
   echo "开始下载 easylist${i}..."
