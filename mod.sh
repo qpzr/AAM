33f8388e225de78fdfc22b274b98b92325625fa0
qpzr <58745620+qpzr@users.noreply.github.com>
2021-10-28 06:21:46 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -7,7 +7,6 @@ cd $(cd "$(dirname "$0")";pwd)
 easylist=(
   "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt"
   "https://raw.githubusercontent.com/easylist/easylistchina/master/easylistchina.txt"
-  "https://raw.githubusercontent.com/easylist/easylist/gh-pages/easylist.txt"
   "https://raw.githubusercontent.com/easylist/easylist/gh-pages/easyprivacy.txt"
   "https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt"
   "https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/combined_original_trackers.txt"
