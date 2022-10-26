efb85eed58450a8a5c0e86ec325386bf139c4e97
qpzr <58745620+qpzr@users.noreply.github.com>
2022-10-27 06:35:08 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -16,13 +16,7 @@ easylist=(
   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_3_Spyware/filter.txt"
   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_14_Annoyances/filter.txt"
   "https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exceptions.txt"
-  "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/privacy.txt"
-  "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/badware.txt"
-  "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/annoyances.txt"
-  "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters.txt"
-  "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2020.txt"
-  "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2021.txt"
-  "https://raw.githubusercontent.com/uBlockOrigin/uAssets/master/filters/filters-2022.txt"
+
 )
 
 hosts=(
