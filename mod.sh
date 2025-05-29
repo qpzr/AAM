022b947349540fe03779be458047dda9b934d2e0
qpzr <58745620+qpzr@users.noreply.github.com>
2025-05-30 02:16:18 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -15,6 +15,7 @@ easylist=(
   "https://raw.githubusercontent.com/AdguardTeam/cname-trackers/master/data/combined_original_trackers.txt"
   "https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt"
   "https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exceptions.txt"
+  "https://raw.githubusercontent.com/qpzr/AAM/main/xunlei.txt"
 )
 
 for i in "${!easylist[@]}"; do
