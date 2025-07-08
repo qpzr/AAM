fcbebf5f7ddc0bdb8a89893c567dfbee9dc3f98a
qpzr <58745620+qpzr@users.noreply.github.com>
2025-07-09 00:56:05 +0800

    Update mod.sh

--- mod.sh
+++ mod.sh
@@ -16,6 +16,7 @@ easylist=(
   'https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt'
   'https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exceptions.txt'
   'https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt'
+  'https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/nocoin.txt'
 )
 
 for i in "${!easylist[@]}"; do
