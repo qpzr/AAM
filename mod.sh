38cbc16897edd4a05bea6878e0c7b0cfdee4d59b
qpzr <58745620+qpzr@users.noreply.github.com>
2026-03-02 05:27:22 +0800

    Add new DNS list source to mod.sh

--- mod.sh
+++ mod.sh
@@ -17,6 +17,7 @@ easylist=(
   'https://raw.githubusercontent.com/AdguardTeam/FiltersRegistry/master/filters/filter_11_Mobile/filter.txt'
   'https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exceptions.txt'
   'https://raw.githubusercontent.com/TG-Twilight/AWAvenue-Ads-Rule/main/AWAvenue-Ads-Rule.txt'
+  'https://raw.githubusercontent.com/damengzhu/banad/refs/heads/main/dnslist.txt'
   'https://raw.githubusercontent.com/hoshsadiq/adblock-nocoin-list/master/nocoin.txt'
 )
 
